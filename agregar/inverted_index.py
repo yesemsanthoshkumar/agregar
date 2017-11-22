aab2d679504bf7668ea79e98045482f90c80c961
yesemsanthoshkumar <yesemsanthoshkumar@gmail.com>
2017-11-22 08:34:34 +0530

    Added basic inverted index class

---
+++ agregar/inverted_index.py
@@ -0,0 +1,18 @@
+"""Inverted index implementation
+to store entities collected from the documents"""
+
+from collections import defaultdict
+
+class InvertedIndex(object):
+    """Inverted index for entities"""
+    def __init__(self):
+        self.idx = defaultdict(lambda: [])
+
+    def add(self, key, elem):
+        """Adds a element to the index at the given key"""
+        print("Appending", elem)
+        self.idx[key].append(elem)
+
+    def count(self, key):
+        """Gives count of particular key"""
+        return len(self.idx[key])
