7a5464e4baf72da100d4caac2ffa0e0b066f706f
yesemsanthoshkumar <yesemsanthoshkumar@gmail.com>
2017-12-04 06:49:11 +0530

    Basic chat flow completed

---
+++ swen/core/swen.py
@@ -0,0 +1,27 @@
+"""Swen chat"""
+
+from flask import Flask, jsonify, request
+
+from processing.message import reply
+
+APP = Flask('SWEN')
+
+@APP.route('/')
+def home():
+    """Homepage"""
+    return jsonify({
+        "status": "success"
+    })
+
+@APP.route('/chat', methods=['POST'])
+def input_message():
+    """Receives input message and sends response message"""
+    response_msg = reply(request.json)
+    return jsonify(response_msg)
+
+if __name__ == '__main__':
+    APP.run(
+        '127.0.0.1',
+        5234,
+        debug=False
+    )
